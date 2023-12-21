0854c317ff9bffd29074a0a3c2b3fa07584c6a61
kecan0406 <kecan0406@gmail.com>
2023-12-21 22:56:42 +0900

    fetch : tone.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from 'react'
-import './App.css'
-import logo from './logo.svg'
-
+import { scheduler } from './lib/scheduler'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  const soundOn = () => {
+    scheduler()
+  }
+  return <button onClick={soundOn}>click me!!</button>
 }
 
 export default App
