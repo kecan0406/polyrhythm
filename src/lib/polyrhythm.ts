7a9c2a9c57456efee31956a02c209d29226ca22f
kecan0406 <kecan0406@gmail.com>
2023-12-31 23:33:24 +0900

    fetch : polyrhythm add draw

--- src/lib/polyrhythm.ts
+++ src/lib/polyrhythm.ts
@@ -1,22 +1,27 @@
 import * as Tone from 'tone'
 import { Volume } from 'tone'
 import { Transport } from 'tone/build/esm/core/clock/Transport'
+import { Draw } from 'tone/build/esm/core/util/Draw'
 
 export class PolyrhythmManager {
   private readonly volume: Volume
   private readonly transport: Transport
+  private readonly draw: Draw
   private repeatNum: number = 0
 
   constructor() {
     Tone.setContext(new Tone.Context({ latencyHint: 'interactive' }))
     this.volume = new Tone.Volume(-20).toDestination()
     this.transport = Tone.getTransport()
+    this.draw = Tone.getDraw()
     Tone.start().then(() => this.transport.start(0))
   }
 
   public repeat() {
     this.repeatNum = this.transport.scheduleRepeat((time) => {
-      console.log(time)
+      this.draw.schedule(() => {
+        console.log(time)
+      }, time)
     }, '1')
   }
 
