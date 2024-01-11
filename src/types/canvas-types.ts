4e72e11ac4ca9baaeba60ab81d221fa3cc03d466
kecan0406 <kecan0406@gmail.com>
2024-01-11 11:03:38 +0900

    refactor : delete useless type

--- src/types/canvas-types.ts
+++ src/types/canvas-types.ts
@@ -1,10 +1,3 @@
 export type Size = { width: number; height: number }
 export type CanvasSize = Size
 export type Point = { x: number; y: number }
-export type XY = [number, number]
-
-export type ClickInteraction = { type: 'click' | 'contextmenu'; value: Point }
-export type WheelInteraction = { type: 'wheelUp' | 'wheelDown'; value: Point }
-export type CanvasInteraction = ClickInteraction | WheelInteraction
-export type ControllerInteraction = ClickInteraction | WheelInteraction
-export type Interaction = CanvasInteraction | ControllerInteraction | null
