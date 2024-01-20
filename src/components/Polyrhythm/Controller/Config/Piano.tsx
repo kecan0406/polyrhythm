b3015172d30ebffbb2413766568d9fc6e1d1e767
kecan0406 <kecan0406@gmail.com>
2024-01-20 18:28:16 +0900

    fetch : piano animation

--- src/components/Polyrhythm/Controller/Config/Piano.tsx
+++ src/components/Polyrhythm/Controller/Config/Piano.tsx
@@ -1,7 +1,11 @@
 import { usePolyrhythmConfig } from '@/hooks/polyrhythm-config-hook'
+import { keyframes } from '@emotion/react'
 import styled from '@emotion/styled'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Note } from 'tone/build/esm/core/type/NoteUnits'
+
+const WHITE_NOTES: Note[] = ['C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5']
+const BLACK_NOTES: Note[] = ['B#5', 'C#5', 'D#5', 'E#5', 'F#5', 'G#5', 'A#5']
 
 const KeyboardContainer = styled.div`
   position: relative;
@@ -9,66 +13,88 @@ const KeyboardContainer = styled.div`
   justify-content: center;
   height: 80px;
 `
+type PianoNoteProps = { none?: boolean; isActive: boolean; index: number }
+const PianoNote = styled.button<PianoNoteProps>`
+  opacity: ${({ none }) => (none ? 0 : 1)};
+  animation: ${({ isActive, index }) => (isActive ? pianoAnimation(3 - index) : 'none')} 0.3s forwards;
+`
+const pianoAnimation = (range: number) => keyframes`
+    from {
+        background-color: #fff;
+    }
+    to {
+        box-shadow: ${range}px -6px 0 0 #17191d inset;
+    }
+`
 
-type NoteProps = { pianoKey?: 'white' | 'black' }
-const Notes = styled.div<NoteProps>`
+type NoteProps = { isBlack: boolean }
+const PianoNotes = styled.div<NoteProps>`
   position: absolute;
   display: flex;
   width: 75%;
   flex-direction: row;
-  height: ${({ pianoKey }) => (pianoKey === 'white' ? '100%' : '55%')};
-`
-const WhiteNote = styled.button<NoteProps>`
-  flex-grow: 1;
-  border: 2px solid #17191d;
-  border-radius: 2px;
-  box-sizing: border-box;
-  background-color: #aaa;
-`
-type BlackNoteProps = { none?: boolean }
-const BlackNote = styled.button<BlackNoteProps>`
-  flex-grow: 1;
-  opacity: ${({ none }) => (none ? 0 : 1)};
-  margin: 0 4px 2px 0;
-  transform: translateX(-50%);
-  border: 2px solid #17191d;
-  border-radius: 2px;
-  box-sizing: border-box;
-  background-color: black;
+  height: ${({ isBlack }) => (isBlack ? 55 : 100)}%;
+  & > ${PianoNote} {
+    flex-grow: 1;
+    border: 2px solid #17191d;
+    border-radius: 2px;
+    box-sizing: border-box;
+    margin: ${({ isBlack }) => isBlack && '0 4px 2px 0'};
+    transform: translateX(${({ isBlack }) => isBlack && -50}%);
+    background-color: ${({ isBlack }) => (isBlack ? 'black' : '#aaa')};
+  }
 `
 
 const Piano = () => {
   const polyrhythmConfig = usePolyrhythmConfig()
   const [note, setNote] = useState<Note>(polyrhythmConfig.note)
-
-  const handlePlayNote = (note: Note) => {
-    setNote(note)
-  }
+  const isPressRef = useRef<boolean>(false)
 
   useEffect(() => {
     polyrhythmConfig.note = note
   }, [note])
 
+  const handleNote = ({ currentTarget }: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    isPressRef.current && setNote(currentTarget.value as Note)
+  }
+
+  const handlePressNote = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    isPressRef.current = true
+    handleNote(e)
+  }
+
   return (
     <KeyboardContainer>
-      <Notes pianoKey="white">
-        <WhiteNote onClick={() => handlePlayNote('C5')} />
-        <WhiteNote onClick={() => handlePlayNote('D5')} />
-        <WhiteNote onClick={() => handlePlayNote('E5')} />
-        <WhiteNote onClick={() => handlePlayNote('F5')} />
-        <WhiteNote onClick={() => handlePlayNote('G5')} />
-        <WhiteNote onClick={() => handlePlayNote('A5')} />
-        <WhiteNote onClick={() => handlePlayNote('B5')} />
-      </Notes>
-      <Notes pianoKey="black">
-        <BlackNote none />
-        <BlackNote onClick={() => handlePlayNote('C#5')} />
-        <BlackNote onClick={() => handlePlayNote('D#5')} />
-        <BlackNote none />
-        <BlackNote onClick={() => handlePlayNote('F#5')} />
-        <BlackNote onClick={() => handlePlayNote('G#5')} />
-        <BlackNote onClick={() => handlePlayNote('A#5')} />
-      </Notes>
+      <PianoNotes isBlack={false}>
+        {WHITE_NOTES.map((pianoNote, index) => (
+          <PianoNote
+            value={pianoNote}
+            key={pianoNote}
+            index={index}
+            isActive={pianoNote === note}
+            onMouseDown={handlePressNote}
+            onMouseUp={() => (isPressRef.current = false)}
+            onMouseMove={handleNote}
+          />
+        ))}
+      </PianoNotes>
+      <PianoNotes isBlack={true}>
+        {BLACK_NOTES.map((pianoNote, index) => {
+          const isNone = index === 0 || index === 3
+          return (
+            <PianoNote
+              value={pianoNote}
+              key={pianoNote}
+              none={isNone}
+              index={index}
+              isActive={pianoNote === note}
+              onMouseDown={handlePressNote}
+              onMouseUp={() => (isPressRef.current = false)}
+              onMouseMove={isNone ? () => {} : handleNote}
+            />
+          )
+        })}
+      </PianoNotes>
     </KeyboardContainer>
   )
 }
